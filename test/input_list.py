717b979f05cbf14300814a3fefc11b7dd9fc2780
Yuta Takaba <takaba.yutya@unipro.co.jp>
2018-09-07 16:59:08 +0900

    TDD2-2

--- test/input_list.py
+++ test/input_list.py
@@ -14,13 +14,9 @@ class InputTest(unittest.TestCase):
 50,50,55"""))
 		self.calc_price = Calc_price()
 
-	# def testPrice(self):
-	# 	self.assertEqual(self.calc_price.calc_price([10, 12]), 24)
-	# 	self.assertEqual(self.calc_price.calc_price([40, 16]), 62)
-	# 	self.assertEqual(self.calc_price.calc_price([100, 45]), 160)
-	# 	self.assertEqual(self.calc_price.calc_price([50, 50, 55]), 171)
-	# 	self.assertEqual(self.calc_price.calc_price([]), 0)
-	# 	self.assertEqual(self.calc_price.calc_price([40, 15]), 61)
-
 	def testInput(self):
 		self.assertEqual(self.input_list.input_list(), [[10, 12], [40, 16], [100, 45], [], [50, 50, 55]])
+
+class OutputTest(unittest.TestCase):
+	def setUp(self):
+		self.output_list = Output_list(io.StringIO(""""""))
