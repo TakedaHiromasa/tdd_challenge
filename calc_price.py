126dbd4c35a00aea5b62701f5d9fd355f3afd82a
TakedaHiromasa <reich893@yahoo.co.jp>
2018-09-07 18:29:12 +0900

    TDD2-2_最終
    無事完成。

--- calc_price.py
+++ calc_price.py
@@ -1,3 +1,7 @@
+import sys
+from input_list import Input_list
+from output_list import Output_list
+
 class Calc_price:
 	def __init__(self):
 		pass
@@ -13,9 +17,19 @@ class Calc_price:
 		return result
 
 	def calc_print(self, input_list):
+		output = []
 		for calc_list in input_list:
-			calc_price(calc_list)
+			output.append(self.calc_price(calc_list))
+		return output
+
+def calc_tax(input_place, output_place):
+	input_list = Input_list(input_place)
+	output_list = Output_list(output_place)
+	calc_price = Calc_price()
+
+	input = input_list.input_list()
+	output = calc_price.calc_print(input)
+	output_list.output_list(output)
 
 if __name__ == '__main__':
-	calc_price = Calc_price()
-	calc_price.calc_print()
+	calc_tax(sys.stdin, sys.stdout)
--- test/input_list.py
+++ test/input_list.py
@@ -1,6 +1,7 @@
 import unittest
 from input_list import Input_list
-from calc_price import Calc_price
+from output_list import Output_list
+from calc_price import Calc_price, calc_tax
 import io
 import os
 
@@ -19,4 +20,31 @@ class InputTest(unittest.TestCase):
 
 class OutputTest(unittest.TestCase):
 	def setUp(self):
-		self.output_list = Output_list(io.StringIO(""""""))
+		self.output = io.StringIO()
+		self.output_list = Output_list(self.output)
+
+	def testOutput(self):
+		self.output = self.output_list.output_list([24, 62, 160, 0, 171])
+		self.assertEqual(self.output.getvalue(),"""24
+62
+160
+0
+171
+""")
+
+class CalcTest(unittest.TestCase):
+	def testCalc(self):
+		input_place = io.StringIO("""10,12
+40,16
+100,45
+
+50,50,55""")
+
+		output_place = io.StringIO()
+		calc_tax(input_place, output_place)
+		self.assertEqual(output_place.getvalue(), """24
+62
+160
+0
+171
+""")
